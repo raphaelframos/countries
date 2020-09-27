0ea1eb89c236612bfddd1333214095dca78add3c
Raphael Ramos <raphaelframos@gmail.com>
2020-09-27 20:10:07 -0300

    Mostrando imagem do tipo svg

--- app/src/main/java/br/com/raphaelframos/countries/MainActivity.kt
+++ app/src/main/java/br/com/raphaelframos/countries/MainActivity.kt
@@ -1,26 +1,26 @@
 package br.com.raphaelframos.countries
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.raphaelframos.countries.adapter.CountryAdapter
 import br.com.raphaelframos.countries.databinding.ActivityMainBinding
 import br.com.raphaelframos.countries.viewmodel.CountryViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-
 class MainActivity : AppCompatActivity() {
 
     private val countryViewModel : CountryViewModel by viewModel()
-    private val adapter = CountryAdapter()
+    private lateinit var adapter : CountryAdapter
     private lateinit var viewBinding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        adapter = CountryAdapter(this)
         initRecyclerView()
         initViewModel()
         setContentView(viewBinding.root)
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     private fun initRecyclerView() {
         viewBinding.recyclerViewCountries.layoutManager = LinearLayoutManager(this)
         viewBinding.recyclerViewCountries.adapter = adapter
+        viewBinding.recyclerViewCountries.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 
     private fun initViewModel() {
--- app/src/main/java/br/com/raphaelframos/countries/adapter/CountryAdapter.kt
+++ app/src/main/java/br/com/raphaelframos/countries/adapter/CountryAdapter.kt
@@ -1,16 +1,19 @@
 package br.com.raphaelframos.countries.adapter
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.raphaelframos.countries.R
 import br.com.raphaelframos.countries.databinding.AdapterCountryBinding
 import br.com.raphaelframos.countries.model.Country
+import com.ahmadrosid.svgloader.SvgLoader
 
-class CountryAdapter : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+class CountryAdapter(private val activity: Activity) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
     private var countries : List<Country> = emptyList()
 
-    class CountryViewHolder(private val viewBinding: AdapterCountryBinding) : RecyclerView.ViewHolder(viewBinding.root) {
+    class CountryViewHolder(private val activity: Activity, private val viewBinding: AdapterCountryBinding) : RecyclerView.ViewHolder(viewBinding.root) {
 
         fun set(country: Country) {
             viewBinding.textViewName.text = country.name
@@ -18,13 +21,17 @@ class CountryAdapter : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() 
             viewBinding.textViewSubregion.text = country.subregion
             viewBinding.textViewCode.text = country.code
             viewBinding.textViewCapital.text = country.capital
+            SvgLoader.pluck()
+                .with(activity)
+                .setPlaceHolder(R.mipmap.ic_launcher, R.mipmap.ic_launcher)
+                .load(country.flag, viewBinding.imageViewFlag);
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val binding = AdapterCountryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CountryViewHolder(binding)
+        return CountryViewHolder(activity, binding)
     }
 
     override fun getItemCount(): Int {
