994a696660a06996e2d136395e9af2013ad5026c
Raphael Ramos <raphaelframos@gmail.com>
2020-09-27 20:21:16 -0300

    Mudando adapter de paises

--- app/src/main/java/br/com/raphaelframos/countries/MainActivity.kt
+++ app/src/main/java/br/com/raphaelframos/countries/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         initRecyclerView()
         initViewModel()
         setContentView(viewBinding.root)
-
     }
 
     private fun initRecyclerView() {
